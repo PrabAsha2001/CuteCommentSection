690bbdb30e3c91f040ba97fba732d07390b29e8d
PrabAsha2001 <umiasha79@gmail.com>
2024-09-11 20:00:55 +0530

    Getting Cuter!!

--- src/pages/Post.js
+++ src/pages/Post.js
@@ -1,22 +1,41 @@
 import React,{useEffect, useState} from 'react'
 import {useParams} from "react-router-dom";
 import axios from 'axios';
-import './home.css';
+import './post.css';
+
 
 
 const Post = () => {
     let {id}=useParams();
     const [postObject,setPostObject]=useState({});
+    const [comments, setComments] = useState([]);
+    const [newComment,setNewComment]=useState("");
+
+
 
     useEffect(()=>{
         axios.get(`http://localhost:3001/Posts/byId/${id}`).then((response )=>{
             setPostObject(response.data);
-          })
-    },[])
+          });
+
+          axios.get(`http://localhost:3001/Comments/${id}`).then((response )=>{
+            setComments(response.data);
+          });
+    },[]);
+
+    const addComment=()=>{
+        axios.post("http://localhost:3001/Comments",{commentBody:newComment,PostId:id})
+        .then((response )=>{
+            const commentToAdd={commentBody:newComment};
+            setComments([...comments,commentToAdd]);
+            setNewComment("")
+        })
+    }
   return (
     <div className='postPage'>
+        
         <div className='LeftSide'>
-            <div className='post'>
+            <div className='Singlepost'>
                 <div className='title'>{postObject.title}</div>
                 <div className='body'>{postObject.PostText}</div>
                 <div className='footer'>
@@ -27,6 +46,27 @@ const Post = () => {
             
         </div>
         <div class="rightSide">
+            <h1>Comment Section</h1>
+            <div className="addCommentContainer">
+              <input
+              onChange={(event)=>{setNewComment(event.target.value)}}
+              type="text" 
+              placeholder='Comments..'
+              value={newComment}
+              />
+              <button
+              onClick={addComment}
+              >Add a Comment</button>
+            </div>
+            <div className='listOfComments'>
+                {comments.map((comment,key)=>{
+                    return <div key={key} className='comment'>{comment.commentBody}
+                   
+                    </div>
+                
+                    
+                })}
+            </div>
         </div>
       
     </div>
